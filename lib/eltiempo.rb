bf18db9ba386cb4b69d69cd579736c1ead6f3f2a
Hamid Khosravian <53334721+hdkhosravian@users.noreply.github.com>
2021-02-02 17:55:15 +0330

    add a comment for .env

--- lib/eltiempo.rb
+++ lib/eltiempo.rb
@@ -10,6 +10,8 @@ require_relative './src/cli/min_average'
 require_relative './src/cli/max_average.rb'
 require_relative './src/cli/today_temperature'
 
+# I prefer to use .env instead of using hardcoding
+# for a gem, we can use an initializer and config it in the project, not inside the code
 Dotenv.load('./.env')
 
 module Lib
